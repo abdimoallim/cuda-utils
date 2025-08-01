f4f565cfb929763020d0bf9fe592711c297e58b5
abdimoallim <moalim.abdi@outlook.com>
2025-08-01 23:12:59 +0300

    feat: CUDA_CHECK, CUDA_CHECK_KERNEL

--- cutils.cuh
+++ cutils.cuh
@@ -1,3 +1,26 @@
+#include <cuda_runtime.h>
+#include <cstdio>
+#include <cstdlib>
+
+#define CUDA_CHECK(call) \
+  do { \
+    cudaError_t err = call; \
+    if (err != cudaSuccess) { \
+      fprintf(stderr, "CUDA error at %s:%d - %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
+      exit(EXIT_FAILURE); \
+    } \
+  } while(0)
+
+#define CUDA_CHECK_KERNEL() \
+  do { \
+    cudaError_t err = cudaGetLastError(); \
+    if (err != cudaSuccess) { \
+      fprintf(stderr, "CUDA kernel error at %s:%d - %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
+      exit(EXIT_FAILURE); \
+    } \
+    CUDA_CHECK(cudaDeviceSynchronize()); \
+  } while(0)
+
 #define DIV_CEIL(a, b) (((a) + (b) - 1) / (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
